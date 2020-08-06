da7febbfd9e778c65f40f6ff4d435720f282d74e
Jiyuan Liu <jiyuan@mrwhoami.com>
2020-08-06 01:43:05 -0700

    Add logger

--- src/main/kotlin/com/mrwhoami/qqservices/Main.kt
+++ src/main/kotlin/com/mrwhoami/qqservices/Main.kt
@@ -1,5 +1,6 @@
 package com.mrwhoami.qqservices
 
+import mu.KotlinLogging
 import net.mamoe.mirai.Bot
 import net.mamoe.mirai.alsoLogin
 import net.mamoe.mirai.join
@@ -10,15 +11,21 @@ import net.mamoe.mirai.event.subscribeMessages
 import net.mamoe.mirai.message.GroupMessageEvent
 import net.mamoe.mirai.message.data.content
 
+private val logger = KotlinLogging.logger {}
+
 suspend fun main() {
     // Login QQ. Use another data class to avoid password tracking.
     val login = BotLoginInfo()
-    val miraiBot = Bot(login.qqId, login.qqPassword).alsoLogin()//新建Bot并登录
+    val miraiBot = Bot(login.qqId, login.qqPassword) {
+        fileBasedDeviceInfo("device.json")
+    }.alsoLogin()
+    logger.info { "${login.qqId} is logged in." }
 
     // Initialize services
     val repeater = Repeater()
 
     miraiBot.subscribeAlways<GroupMessageEvent> { event ->
+        logger.info { "Grp [${event.group.id}][${event.sender.id}]：${event.message.content}" }
         // repeater behaviour
         val reply_msg = repeater.recvGrpMsg(event.group.id, event.sender.id, event.message.content)
         if (reply_msg != null) {
